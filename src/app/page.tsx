0e3260ad30d2e00ec51020aa08599a7d08ea3fee
Louie Alfonso <lfriasalfonso@gmail.com>
2024-06-20 17:30:19 +0100

    NextJs Project for Oya Renewables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 
-export default function Home() {
-  return (
-    <main >
-      
-    </main>
-  );
+import Wrapper from '@/layouts/Wrapper';
+import React from 'react';
+
+export const metadata = {
+  title: 'Renewable Energy Company - Clean Energy Solutions Next js Template',
+  description: 'The Solar Panel React Next js Project with modern and responsive design specifically created for websites related to solar energy and renewable energy solutions',
 }
+
+
+const MainHome = () => {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+};
+
+export default MainHome;
